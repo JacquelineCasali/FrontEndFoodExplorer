4c4bdfdeb2a919af8c9bcf40827a1f62a9d5eb38
Rodrigo <drigodeveloper@gmail.com>
2023-11-11 17:12:47 -0300

    Construindno o componente header

--- src/components/header/Index.jsx
+++ src/components/header/Index.jsx
@@ -1,4 +1,4 @@
-import { Container } from "./styles";
+import { Container, Logo } from "./styles";
 import { FiMenu } from 'react-icons/fi'
 import { BsFillHexagonFill, BsSearch } from 'react-icons/bs'
 import { PiReceipt, PiSignOutFill } from 'react-icons/pi'
@@ -6,7 +6,25 @@ import { PiReceipt, PiSignOutFill } from 'react-icons/pi'
 export function Header() {
     return(
         <Container>
+            <Logo>
+                <BsFillHexagonFill />
+                <h1>Food explorer</h1>
+            </Logo>
 
+            <input
+            placeholder="Busque por pratos ou ingredientes" 
+            type="text" 
+            />
+
+            <button className="buttonShoppBag">
+                <PiReceipt/>
+                Pedidos(0)
+
+            </button>
+
+            <button className="buttonLogout">
+                <PiSignOutFill />
+            </button>
         </Container>
     )
 }
--- src/components/header/styles.js
+++ src/components/header/styles.js
@@ -6,4 +6,81 @@ export const Container = styled.div`
     width: 100%;
     height: 7.1rem;
     background-color: ${({theme}) => theme.COLORS.BACKGROUND_400};
+    padding: 1.5rem 7.6rem;
+    align-items: center;
+    border-radius: .3rem;
+
+
+    > input {
+        width: 100%;
+        height: 3rem;
+        margin-right: 2rem;
+        border-radius: .3rem;
+        border: none;
+        background-color: ${({theme}) => theme.COLORS.BACKGROUND_700};
+        padding: 0.7rem 0.8rem;
+
+   }
+
+   > .buttonShoppBag {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+
+        gap: .5rem;
+        width: 13.5rem;
+        height: 3rem;
+
+        border-radius: .3rem;
+        border: none;
+        margin-right: 2rem;
+
+        color: ${({theme}) => theme.COLORS.WHITE};
+        background-color: ${({theme}) => theme.COLORS.RED};
+
+        font-family: 'Poppins', sans-serif;
+        font-size: 14px;
+        font-style: normal;
+        font-weight: 500;
+
+        >svg {
+            width: 2rem;
+            height: 2rem;
+        }
+
+
+   }
+
+   > .buttonLogout {
+    border: none;
+    background-color: transparent;
+
+
+    > svg {
+        width: 2rem;
+        height: 2rem;
+        color: ${({theme}) => theme.COLORS.WHITE};
+    }
+   }
+`;
+
+export const Logo = styled.div`
+    display: flex;
+    width: 12.31rem;
+
+
+    > svg {
+        width: 1.8rem;
+        height: 1.8rem;
+        color: ${({theme}) => theme.COLORS.BLUE};
+
+    }
+
+    > h1 {
+        font-family: 'Roboto', sans-serif;
+        font-size: 1.5rem;
+        font-style: normal;
+        font-weight: 700;
+    }
+
 `;
