77d1f503d951bed425317af2e55b0362b4fde811
Jacqueline <103325619+JacquelineCasali@users.noreply.github.com>
2023-11-13 07:12:51 -0300

    Update Index.jsx

--- src/pages/Home/Index.jsx
+++ src/pages/Home/Index.jsx
@@ -1,15 +1,19 @@
 import { Container, Banner} from "./styles";
 import { Header} from "../../components/header";
-
+import logo from "../../assets/logo.svg"
+import maskgroup from "../../assets/Maskgroup-1.png"
 
 export function Home() {
-    return(
+  
+    
+        return(
         <Container>
             <Header />
 
             <Banner>
-                <img src="../../assets/logo.svg" alt="" />
-
+                {/* <img src="../../assets/logo.svg" alt="" /> */}
+                <img src={logo} alt=''/>
+                <img src={maskgroup} alt=''/>
                 <div className="description">
                     <h1>Sabores inigualáveis</h1>
                     <p>Sinta o cuidado do preparo com ingredientes selecionados</p>
